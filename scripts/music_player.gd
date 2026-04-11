444d580aad46227db707659b6fe68bfd9d531b48
Trulle1234 <leonard.trulsson@gmail.com>
2026-04-11 16:53:54 +0200

    had to be done i cound not figure that out

--- scripts/music_player.gd
+++ scripts/music_player.gd
@@ -1,30 +1,18 @@
 extends AudioStreamPlayer2D
 
-const MUSIC = [
-	"res://assets/rossini.mp3",
-	"res://assets/vivaldi.mp3"
-]
 
 func _ready() -> void:
 	randomize()
-
-	if not finished.is_connected(_on_music_finished):
-		finished.connect(_on_music_finished)
-
+	self.finished.connect(_on_music_finished)
 	play_random_music()
 
 func play_random_music() -> void:
-	if playing:
-		stop()
+	var path = "res://assets/rossini.mp3"
+	var stream = load(path) as AudioStream
 
-	var path = MUSIC[randi() % MUSIC.size()]
-	stream = load(path) as AudioStream
+	self.stream = stream
 
-	var length := stream.get_length()
-	var max_start = max(length - 30.0, 0.0)
-	var start_pos = randf_range(0.0, max_start)
-
-	play(start_pos)
+	self.play()
 
 func _on_music_finished() -> void:
 	play_random_music()
